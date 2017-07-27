071be34db6eb736ec8b9998fc14f6bd2d2f75865
Paweł Kopiczko <kopiczko@gmail.com>
2017-07-27 11:14:23 +0200

    fix comments

--- error.go
+++ error.go
@@ -6,22 +6,15 @@ import (
 
 var InvalidConfigError = errgo.New("invalid config")
 
-// IsInvalidConfig represents the error matcher for public use. Code using the
-// storage internally should use this public key matcher to verify if some
-// storage error is of type "key not found", instead of using a specific error
-// matching of some specific storage implementation. This public error matcher
-// groups all necessary error matchers of more specific storage
-// implementations.
+// IsInvalidConfig asserts InvalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return errgo.Cause(err) == InvalidConfigError
 }
 
 var NotFoundError = errgo.New("not found")
 
-// IsNotFound represents the error matcher for public use. Code using the
-// storage internally should use this public key matcher to verify if some
-// storage error is of type "key not found", instead of using a specific error
-// matching of some specific storage implementation.
+// IsNotFound asserts NotFoundError. The library user's code should use this
+// public key matcher to verify if some storage error is of type NotFoundError.
 func IsNotFound(err error) bool {
 	return errgo.Cause(err) == NotFoundError
 }
