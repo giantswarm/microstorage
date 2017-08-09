41991622bcff6ef1aec4be769f0fc7417e9f662c
Paweł Kopiczko <kopiczko@gmail.com>
2017-08-09 12:26:35 +0200

    key funcs godoc

--- key_funcs.go
+++ key_funcs.go
@@ -7,7 +7,12 @@ import (
 )
 
 // SanitizeKey ensures the key has leading slash and does not have trailing
-// slash. It fails with InvalidKeyError when key is invalid.
+// slash. It fails with InvalidKeyError when key is invalid, i.e. key that is
+// sanitized is always valid.
+//
+// This function is meant to be used by Storage implementations to simplify key
+// validation logic and potentially implementation logic, because it reduces
+// number of invariants to check greatly.
 func SanitizeKey(key string) (string, error) {
 	if !isValidKey(key) {
 		return "", microerror.Maskf(InvalidKeyError, "key=%s", key)
@@ -23,8 +28,11 @@ func SanitizeKey(key string) (string, error) {
 	return key, nil
 }
 
-// isValidKey check if this storage key is valid, i.e. does not contain double
+// isValidKey checks if this storage key is valid, i.e. does not contain double
 // slashes, is not empty, and does not contain only slashes.
+//
+// NOTE: Valid key is not a sanitized key. But the key must be valid in order
+// to be sanitized.
 func isValidKey(key string) bool {
 	if key == "" || key == "/" {
 		return false
