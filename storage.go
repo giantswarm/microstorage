0a3499cd9909d42e4238b8379b667fc40fa9a957
Paweł Kopiczko <kopiczko@gmail.com>
2017-08-23 16:08:36 +0200

    introduce KV struct (#18)
    - Introduced KV struct.
    - Opens possibility for Put to take multiple KVs.
    - List returns KVs instead of keys-only. This prevents from doing another
      call when it's necessary to also know key value. Useful in userd for
      example. Also useful in data migration optimization.
    - Search returns KV to be on par with List.

--- storage.go
+++ storage.go
@@ -4,19 +4,30 @@ import (
 	"context"
 )
 
-// Storage represents the abstraction for underlying storage backends. A storage
-// implementation does not care about specific data types. All the
-// storage cares about are key-value pairs. Code making use of the storage
-// have to take care about specific types they care about them self.
+// KV is a key-value pair.
+type KV struct {
+	// Key should take a format of path separated by slashes "/". E.g.
+	// "/a/b/c". Storage implementations should validate the key before
+	// storing them. Key sanitized before inserting to the storage. I.e.
+	// leading slash can be added and trailing slash can be removed. E.g.
+	// "/a/b/c", "a/b/c/", "a/b/c/", and "/a/b/c/" represent the same key.
+	Key string
+	// Val is an arbitrary value stored under the Key.
+	Val string
+}
+
+func NewKV(key, val string) KV {
+	return KV{
+		Key: key,
+		Val: val,
+	}
+}
+
+// Storage represents the abstraction for underlying storage backends.
 type Storage interface {
 	// Put stores the given value under the given key. If the value
-	// under the key already exists Put overrides it. Keys and values might
-	// have specific schemes depending on the specific storage
-	// implementation.  E.g. an etcd storage implementation will allow keys
-	// to be defined as paths: path/to/key. Values might be JSON strings in
-	// case the storage implementation wants to store its data as JSON
-	// strings.
-	Put(ctx context.Context, key, value string) error
+	// under the key already exists Put overrides it.
+	Put(ctx context.Context, kv KV) error
 	// Delete removes the value stored under the given key.
 	Delete(ctx context.Context, key string) error
 	// Exists checks if a value under the given key exists or not.
@@ -24,7 +35,7 @@ type Storage interface {
 	// List does a lookup for all keys stored under the key, and returns the
 	// relative key path, if any.
 	// E.g: listing /foo/, with the key /foo/bar, returns bar.
-	List(ctx context.Context, key string) ([]string, error)
+	List(ctx context.Context, key string) ([]KV, error)
 	// Search does a lookup for the value stored under key and returns it, if any.
-	Search(ctx context.Context, key string) (string, error)
+	Search(ctx context.Context, key string) (KV, error)
 }
